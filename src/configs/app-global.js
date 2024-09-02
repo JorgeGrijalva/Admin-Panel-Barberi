e0e1c34c5a3ad377b18f4fa35bc4b8ea80089f09
Arturo Grijalva <arturogrijalva@MacBook-Pro-de-Arturo.local>
2024-09-01 18:31:59 -0600

    API keys firebase

--- src/configs/app-global.js
+++ src/configs/app-global.js
@@ -5,24 +5,23 @@ export const api_url = BASE_URL + '/api/v1/';
 export const api_url_admin = BASE_URL + '/api/v1/dashboard/admin/';
 export const api_url_admin_dashboard = BASE_URL + '/api/v1/dashboard/';
 export const IMG_URL = '';
-export const MAP_API_KEY = 'AIzaSyBbfnvObzwcs6OLXOmSaG9CtIhwjmBwLTQ';
+export const MAP_API_KEY = 'AIzaSyAl4jwOnTYtBebfV2UwecQKZ8OBytyEL0o';
 export const export_url = BASE_URL + '/storage/';
 export const example = BASE_URL + '/';
 export const defaultCenter = { lat: 28.63528, lng: -106.08889 };
 
 export const VAPID_KEY =
-  'BHDzocwltCr-emzw3egEMs3ycWdylN7G3MQenxXB6B7DQe7azOGySEyUQc4OAW_3vR9xMf4nAq71ljvgJO6RNik';
-export const API_KEY = 'AIzaSyAPKaWFljUvqmmt-Rln4W5_ngApOm7Pgsc';
-export const AUTH_DOMAIN = 'barberi-b72dd.firebaseapp.com';
-export const PROJECT_ID = 'barberi-b72dd';
-export const STORAGE_BUCKET = 'barberi-b72dd.appspot.com';
-export const MESSAGING_SENDER_ID = '642138481701';
-export const APP_ID = '1:642138481701:web:2e5ce28bd985116bc9cbcc';
-export const MEASUREMENT_ID = 'G-BGWQB141JG';
-export const DYNAMIC_LINK_DOMAIN = 'https://ibeautyapp.page.link';
-export const ANDROID_PACKAGE_NAME = 'org.barberi';
-export const IOS_BUNDLE_ID = 'com.barberi';
-
+  'BOvo_kB65sYCNpqfeMvuWtiQ_opszQCAV85DazCegNnh5OiGeH2W3L4lmab-xOeHh5p63ncDtBIrQEOcLNxZjvI';
+export const API_KEY = 'AIzaSyDt3zjCrQuVXHAF0jegYviSqfN4lIqVo4c';
+export const AUTH_DOMAIN = 'barberi-e15ce.firebaseapp.com';
+export const PROJECT_ID = 'barberi-e15ce';
+export const STORAGE_BUCKET = 'barberi-e15ce.appspot.com';
+export const MESSAGING_SENDER_ID = '216141489123';
+export const APP_ID = '1:216141489123:web:09bb56f3db3bc63e5df6d6';
+export const MEASUREMENT_ID = 'G-12H7BTJPNB';
+export const DYNAMIC_LINK_DOMAIN = 'https://barberi.page.link';
+export const ANDROID_PACKAGE_NAME = 'com.barberi.app';
+export const IOS_BUNDLE_ID = 'ios.barberi.app';
 export const RECAPTCHASITEKEY = '6LdvEgMqAAAAAGo2_KX8HmnO-24HC7W1IgtMaSBK';
 
 export const DEMO_SELLER = 107; // seller_id
